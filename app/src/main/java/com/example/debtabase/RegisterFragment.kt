1dd991e84b0b0b28fd04be0113cd8f5377eae846
Giwithx <bielzagio@gmail.com>
2022-11-03 08:57:40 +0800

    Version 1.01

--- app/src/main/java/com/example/debtabase/RegisterFragment.kt
+++ app/src/main/java/com/example/debtabase/RegisterFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.debtabase.databinding.FragmentRegisterBinding
+import com.example.debtabase.databinding.FragmentWelcomeBinding
 
 
 class RegisterFragment : Fragment() {
@@ -14,7 +18,13 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
+            inflater, R.layout.fragment_register, container, false
+        )
+        binding.btnSave.setOnClickListener{ view: View ->
+            view.findNavController().navigate(R.id.action_registerFragment_to_welcomeFragment2)
+        }
+        return binding.root
     }
 
 }
