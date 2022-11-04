ae44baa660c7bac0fe456e274168ab0a0852f41f
Giwithx <bielzagio@gmail.com>
2022-11-04 20:33:06 +0800

    Added cancel navigation in SMSFragment.kt

--- app/src/main/java/com/example/debtabase/SMSFragment.kt
+++ app/src/main/java/com/example/debtabase/SMSFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.debtabase.databinding.FragmentSMSBinding
 
 
 class SMSFragment : Fragment() {
@@ -17,7 +20,13 @@ class SMSFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         (activity as AppCompatActivity).supportActionBar?.title = "Send SMS"
-        return inflater.inflate(R.layout.fragment_s_m_s, container, false)
+        val binding = DataBindingUtil.inflate<FragmentSMSBinding>(
+            inflater, R.layout.fragment_s_m_s, container, false
+        )
+        binding.btnCancelSend.setOnClickListener{ view: View ->
+            view.findNavController().navigate(R.id.action_SMSFragment_to_homeFragment)
+        }
+        return binding.root
     }
 
 }
