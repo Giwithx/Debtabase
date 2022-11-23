257c33f42b032df430d7b1181d89880e17ffa4b9
Giwithx <bielzagio@gmail.com>
2022-11-23 19:44:18 +0800

    Version 1.3.4 - Debt List Calendar now working

--- app/src/main/java/com/example/debtabase/DebtActivity.kt
+++ app/src/main/java/com/example/debtabase/DebtActivity.kt
@@ -1,10 +1,13 @@
 package com.example.debtabase
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
+import java.util.*
 import kotlin.collections.ArrayList
 
 
@@ -13,13 +16,15 @@ class DebtActivity : AppCompatActivity() {
     private lateinit var DebtSpinner: Spinner
     private lateinit var DebtProduct: EditText
     private lateinit var DebtProdPrice: EditText
-    private lateinit var DebtDueDate: EditText
+    private lateinit var tvDate: TextView
+    private lateinit var btnDate: Button
     private lateinit var btnAdd: Button
-    private lateinit var btnSave: Button
+    private lateinit var btnClear: Button
     private lateinit var spinnerList: ArrayList<String>
     private lateinit var adapter: ArrayAdapter<String>
     private lateinit var DebtContainer: String
-
+    private lateinit var DateContainer: String
+    private lateinit var Counter: String
 
     private lateinit var dbRefReg: DatabaseReference
     private lateinit var dbRefDebt: DatabaseReference
@@ -28,6 +33,7 @@ class DebtActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debt)
         val actionbar = supportActionBar
+
         actionbar!!.title = "Debt"
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
@@ -38,11 +44,12 @@ class DebtActivity : AppCompatActivity() {
         DebtSpinner = findViewById(R.id.textInputLayout2)
         DebtProduct = findViewById(R.id.txtProdName)
         DebtProdPrice = findViewById(R.id.txtPricesInp)
-        DebtDueDate = findViewById(R.id.txtDateInp)
+        tvDate = findViewById(R.id.tvDatePicker)
+        btnDate = findViewById(R.id.btnDatePicker)
         btnAdd = findViewById(R.id.btnAdd)
-        btnSave = findViewById(R.id.btnSaveProd)
+        btnClear = findViewById(R.id.btnClearData)
 
-
+        Counter = ""
         spinnerList = ArrayList()
         adapter = ArrayAdapter(this@DebtActivity, android.R.layout.simple_spinner_dropdown_item,
             spinnerList
@@ -51,74 +58,81 @@ class DebtActivity : AppCompatActivity() {
         DebtSpinner.adapter = adapter
         ShowData()
 
-        btnSave.setOnClickListener {
-            saveDebtData()
+        btnDate.setOnClickListener {
+            datepicker()
+        }
+
+        btnClear.setOnClickListener {
+            if(::DebtContainer.isInitialized && ::DateContainer.isInitialized){
+                clearDebtData()
+            }
+            else{
+                Toast.makeText(this@DebtActivity, "Please input data", Toast.LENGTH_SHORT).show()
+            }
         }
         btnAdd.setOnClickListener {
-            addDebtData()
-        }
-
-    }
-    private fun saveDebtData(){
-        val DebtFN = DebtContainer
-        val DebtPrice = DebtProdPrice.text.toString()
-        val DebtDate = DebtDueDate.text.toString()
-        val DebtProdList = DebtProduct.text.toString()
-
-        if(DebtProdList.isEmpty()){
-            DebtProduct.error = "Please enter product."
-        }
-        if(DebtPrice.isEmpty()){
-            DebtProdPrice.error = "Please enter product price"
-        }
-        if(DebtDate.isEmpty()){
-            DebtDueDate.error = "Please enter due date."
-        }
-
-        val debtlist = CustomerDebtModel(DebtFN, DebtPrice, DebtDate)
-        val debtprod = CustomerProdModel(DebtProdList)
-
-        if(DebtPrice.isNotEmpty() && DebtDate.isNotEmpty()){
-            dbRefDebt.child(DebtFN).child(DebtDate).setValue(debtlist).addOnCompleteListener {
-                Toast.makeText(this, "Data Successfully added.", Toast.LENGTH_LONG).show()
-                DebtProdPrice.text.clear()
-                DebtDueDate.text.clear()
-                if(DebtProdList.isNotEmpty()){
-                    dbRefDebt.child(DebtFN).child(DebtDate).child("debtProd").push().setValue(debtprod).addOnCompleteListener {
-                        DebtProduct.text.clear()
-                    }.addOnFailureListener { err2 ->
-                        Toast.makeText(this, "Error ${err2.message}", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }.addOnFailureListener { err ->
-                Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
+            if(::DebtContainer.isInitialized && ::DateContainer.isInitialized){
+                addDebtData()
+            }
+            else{
+                Toast.makeText(this@DebtActivity, "Please input data", Toast.LENGTH_SHORT).show()
             }
         }
 
+    }
+    private fun datepicker(){
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val dateText = tvDate
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, myear, mmonth, mday ->
+            dateText.text = "$myear-${mmonth+1}-$mday"
+            DateContainer = dateText.text.toString()
+        },year,month,day).show()
     }
     private fun addDebtData(){
         val DebtFN = DebtContainer
         val DebtPrice = DebtProdPrice.text.toString()
+        val DebtDate = DateContainer
         val DebtProdList = DebtProduct.text.toString()
-        val DebtDate = DebtDueDate.text.toString()
+        val Date = tvDate
         if(DebtProdList.isEmpty()){
             DebtProduct.error = "Please enter product."
         }
         if(DebtPrice.isEmpty()){
             DebtProdPrice.error = "Please enter product price"
         }
-        if(DebtDate.isEmpty()){
-            DebtDueDate.error = "Please enter due date."
+        if (Date.text.isEmpty() || Date.text.equals("Date")){
+            Toast.makeText(this@DebtActivity, "Please input Date", Toast.LENGTH_SHORT).show()
         }
-        val debtprod = CustomerProdModel(DebtProdList)
+        val debtlist = CustomerDebtModel(DebtDate,DebtPrice)
+        val debtprod = CustomerDebtListModel(DebtProdList)
+        if(DebtPrice.isNotEmpty() && DebtDate.isNotEmpty()&& Counter !="1"){
+            dbRefDebt.child(DebtFN).child(DebtDate).setValue(debtlist).addOnCompleteListener {
+                Toast.makeText(this, "Data Successfully added.", Toast.LENGTH_LONG).show()
+                DebtProdPrice.text.clear()
+                Counter = "1"
+            }.addOnFailureListener { err ->
+                Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
+            }
+        }
         if(DebtProdList.isNotEmpty() && DebtPrice.isNotEmpty()){
             dbRefDebt.child(DebtFN).child(DebtDate).child("debtProd").push().setValue(debtprod).addOnCompleteListener {
                 DebtProduct.text.clear()
-                DebtProdPrice.text.clear()
             }.addOnFailureListener { err3 ->
                 Toast.makeText(this, "Error ${err3.message}", Toast.LENGTH_LONG).show()
             }
         }
+
+    }
+    private fun clearDebtData(){
+        val Date = tvDate
+
+        Date.setText("Date")
+        DebtProdPrice.text.clear()
+        DebtProduct.text.clear()
+        Counter = ""
     }
     private fun ShowData() {
         dbRefReg!!.addValueEventListener(object : ValueEventListener {
